be9a615af8684bd86cf1118a2f92f44135a80ace
vanessuniq <vanessuniq@gmail.com>
2021-03-07 15:33:27 -0500

    manually creating routes controller and using explicit rendering

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -6,5 +6,21 @@ class DemoController < ApplicationController
     @numbers = [1,2,3,4,5]
     # redirect_to action: 'index'
   end
+
+  def about
+    render "about_us"
+  end
+  
+  def contact
+    @phone_number = "+1 (940)-444-4545"
+    @country = params[:country]
+    if @country == "ca" || @country == "us"
+      @phone_number = "(800)-555-1008"
+    elsif @country == "uk"
+      @phone_number = "(020)-108-0111"
+    end
+    render "contact_us"
+  end
+  
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root 'demo#index'
   get 'demo/index'
   get 'demo/hello'
+  get 'demo/about'
+  get 'demo/contact'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
