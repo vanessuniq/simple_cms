c1ac2c68889e403d3152abff696baa64d1028ea6
vanessuniq <vanessuniq@gmail.com>
2021-03-11 01:37:05 -0500

    refactor routes to use resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,20 @@
 Rails.application.routes.draw do
-  get 'pages/new'
-  get 'pages/index'
-  get 'pages/show'
-  get 'pages/edit'
-  get 'subjects/new'
-  get 'subjects/index'
-  get 'subjects/show'
-  get 'subjects/edit'
   root 'demo#index'
   get 'demo/index'
   get 'demo/hello'
   get 'demo/about'
   get 'demo/contact'
+
+  resources :pages
+  resources :subjects
+  # get 'pages/new'
+  # get 'pages/index'
+  # get 'pages/show'
+  # get 'pages/edit'
+
+  # get 'subjects/new'
+  # get 'subjects/index'
+  # get 'subjects/show'
+  # get 'subjects/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
