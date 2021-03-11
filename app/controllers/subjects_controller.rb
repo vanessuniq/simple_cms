1546d8a04a3f26eadca9841eb518e0911fa9c90c
vanessuniq <vanessuniq@gmail.com>
2021-03-11 16:39:16 -0500

    set subject params to prevent cross site attack, and method to find a subject

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,4 +1,5 @@
 class SubjectsController < ApplicationController
+  before_action :find_subject, only: [:show, :edit, :update, :destroy]
   # Create
   def new
 
@@ -24,4 +25,17 @@ class SubjectsController < ApplicationController
   def destroy
     
   end
+
+  private
+
+  def subject_params
+    params.require(:subject).permit(:name, :position, :visible)
+  end
+  
+  def find_subject
+    @subject = Subject.find_by(id: params[:id])
+
+    redirect_to :back unless @subject 
+  end
+  
 end
