0bd040e1f43862edd552ddceeaca1d8f858b4080
vanessuniq <vanessuniq@gmail.com>
2021-03-13 16:17:01 -0500

    cascade destroy of dependents

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,5 +1,5 @@
 class Subject < ApplicationRecord
-    has_many :pages
+    has_many :pages, dependent: :destroy
     validates :name, presence: true, uniqueness: { case_sensitive: false }, length: {maximum: 50, minimum: 4}
     validates_presence_of :position, on: :create, message: "can't be blank"
     
