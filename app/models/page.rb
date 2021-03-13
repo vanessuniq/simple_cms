40694bc5ba08ecdf6986fb03044e628d9f33ab77
vanessuniq <vanessuniq@gmail.com>
2021-03-13 14:54:55 -0500

    added validations to page

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,3 +1,7 @@
 class Page < ApplicationRecord
   belongs_to :subject
+  validates_presence_of :position, :visible, :subject_id
+  validates :name, presence: true, uniqueness: { case_sensitive: false }, length: {maximum: 50, minimun: 4}
+  validates :content, presence: true, length: {minimun: 10}
+  
 end
