354623f334bcd0905797224218029eb35edd80b0
vanessuniq <vanessuniq@gmail.com>
2021-03-13 15:09:09 -0500

    added subject_id field to page form

--- app/models/page.rb
+++ app/models/page.rb
@@ -2,7 +2,7 @@ class Page < ApplicationRecord
   belongs_to :subject
   validates_presence_of :position, :visible, :subject_id
   validates_uniqueness_of :position, scope: :subject
-  validates :name, presence: true, uniqueness: { case_sensitive: false }, length: {maximum: 50, minimun: 4}
+  validates :name, presence: true, uniqueness: { case_sensitive: false }, length: {maximum: 50, minimum: 4}
   validates :content, presence: true, length: {minimum: 10}
   
 end
