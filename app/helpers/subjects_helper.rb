1d8bc44d0e85a7af7163d1baabb1cd6f7de4d8b9
vanessuniq <vanessuniq@gmail.com>
2021-03-12 22:20:20 -0500

    created form errors helper to display errors from creating or updating an object

--- app/helpers/subjects_helper.rb
+++ app/helpers/subjects_helper.rb
@@ -5,8 +5,16 @@ module SubjectsHelper
         end
         .join(" ").html_safe
     end
+
     def visible_options
         [{value: true, name: "yes"}, {value: false, name: "no"}]
+    end
+
+    def display_form_errors(object)
+        object.errors.full_messages.map do |msg|
+            content_tag(:p, msg)
+        end
+        .join(" ").html_safe
     end
     
 end
