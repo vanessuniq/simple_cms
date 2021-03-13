6b5a85103cccb56fe92ea47b9a0220d0778e1b6c
vanessuniq <vanessuniq@gmail.com>
2021-03-12 23:25:53 -0500

    moved errors helper to Application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+    def display_form_errors(object)
+        object.errors.full_messages.map do |msg|
+            content_tag(:p, msg)
+        end
+        .join(" ").html_safe
+    end
 end
--- app/helpers/subjects_helper.rb
+++ app/helpers/subjects_helper.rb
@@ -9,12 +9,5 @@ module SubjectsHelper
     def visible_options
         [{value: true, name: "yes"}, {value: false, name: "no"}]
     end
-
-    def display_form_errors(object)
-        object.errors.full_messages.map do |msg|
-            content_tag(:p, msg)
-        end
-        .join(" ").html_safe
-    end
     
 end
