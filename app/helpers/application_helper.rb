9c617edd13cd1b3dd28fd60b3c6fd83a3ffd654e
vanessuniq <vanessuniq@gmail.com>
2021-03-13 15:33:19 -0500

    refactor display forms errors helper by adding errors partial and rendering it

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,16 @@
 module ApplicationHelper
     def display_form_errors(object)
-        object.errors.full_messages.map do |msg|
-            content_tag(:p, msg)
+        render(partial: "application/form_errors", locals: {object: object})
+    end
+
+    def display_message
+        flash.map do |k, v|
+            content_tag(:p, v, class: k)
         end
         .join(" ").html_safe
+    end
+
+    def visible_options
+        [{value: true, name: "yes"}, {value: false, name: "no"}]
     end
 end
--- app/helpers/subjects_helper.rb
+++ app/helpers/subjects_helper.rb
@@ -1,13 +1,2 @@
 module SubjectsHelper
-    def display_message
-        flash.map do |k, v|
-            content_tag(:p, v, class: k)
-        end
-        .join(" ").html_safe
-    end
-
-    def visible_options
-        [{value: true, name: "yes"}, {value: false, name: "no"}]
-    end
-    
 end
