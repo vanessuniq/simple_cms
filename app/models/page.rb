854f2ff132e89c4355faa303a307aba7ed23e3a8
vanessuniq <vanessuniq@gmail.com>
2021-03-13 14:55:48 -0500

    debug typo

--- app/models/page.rb
+++ app/models/page.rb
@@ -2,6 +2,6 @@ class Page < ApplicationRecord
   belongs_to :subject
   validates_presence_of :position, :visible, :subject_id
   validates :name, presence: true, uniqueness: { case_sensitive: false }, length: {maximum: 50, minimun: 4}
-  validates :content, presence: true, length: {minimun: 10}
+  validates :content, presence: true, length: {minimum: 10}
   
 end
